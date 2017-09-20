dac839c0b6089a023e45f4a97313e2da885a5e76
juanfparra12 <31254711+juanfparra12@users.noreply.github.com>
2017-09-19 22:17:16 -0400

    Update config.example.js

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -1,13 +1,13 @@
-//This file holds any configuration variables we may need 
+//This file holds any configuration variables we may need
 //'config.js' is ignored by git to protect sensitive information, such as your database's username and password
 //copy this file's contents to another file 'config.js' and store your MongoLab uri there
 
 module.exports = {
   db: {
-    uri: '', //place the URI of your mongo database here.
-  }, 
+    uri: 'mongodb://juanfparra12:examplepassword@ds121464.mlab.com:21464/assignment_3', //place the URI of your mongo database here.
+  },
   googleMaps: {
-    key: ''
+    key: 'AIzaSyAF3jl5UgQSjmRuSTJ_lDx7cutIp-fA0Tk'
   },
   port: 8080
 };
