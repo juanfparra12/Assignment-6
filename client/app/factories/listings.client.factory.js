9e0ceb07fc0269a3d8c48413b71014793a349ab7
juanfparra12 <31254711+juanfparra12@users.noreply.github.com>
2017-09-22 22:36:02 -0400

    Update listings.client.factory.js

--- client/app/factories/listings.client.factory.js
+++ client/app/factories/listings.client.factory.js
@@ -2,23 +2,23 @@ angular.module('listings').factory('Listings', ['$http',
   function($http) {
     var methods = {
       getAll: function() {
-        return $http.get('cen3031assignment-6.herokuapp.com/api/listings');
+        return $http.get('https://cen3031assignment-6.herokuapp.com/api/listings');
       },
 
       create: function(listing) {
-        return $http.post('cen3031assignment-6.herokuapp.com/api/listings', listing);
+        return $http.post('https://cen3031assignment-6.herokuapp.com/api/listings', listing);
       }, 
 
       read: function(id) {
-        return $http.get('cen3031assignment-6.herokuapp.com/api/listings' + id);
+        return $http.get('https://cen3031assignment-6.herokuapp.com/api/listings' + id);
       }, 
 
       update: function(id, listing) {
-        return $http.put('cen3031assignment-6.herokuapp.com/api/listings' + id, listing);
+        return $http.put('https://cen3031assignment-6.herokuapp.com/api/listings' + id, listing);
       }, 
 
       delete: function(id) {
-        return $http.delete('cen3031assignment-6.herokuapp.com/api/listings + id);
+        return $http.delete('https://cen3031assignment-6.herokuapp.com/api/listings' + id);
       }
     };
 
